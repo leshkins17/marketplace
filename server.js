0d84249dbcbef26a3dbecf638bc6fc93d2c27a21
lemonade1717 <gaastreet@gmail.com>
2025-10-08 23:34:55 +0200

    pages and routes

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
+app.get("/market", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "market.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "post.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
